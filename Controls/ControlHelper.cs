d8fe2eb6ed8c1ece08568532f4d277fc05ff6c9c
IvanGit <5630565+IvanGit@users.noreply.github.com>
2025-01-19 15:41:49 +0300

    v0.3.1

---
+++ Controls/ControlHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace System.Windows.Controls
+{
+    public static class ControlHelper
+    {
+        public static void ClearStyle(this Control? control)
+        {
+            if (control is null)
+            {
+                return;
+            }
+
+            control.Template = null;
+            control.Style = null;
+        }
+    }
+}
